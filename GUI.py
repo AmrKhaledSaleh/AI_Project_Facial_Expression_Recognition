a5f60dbcd7c7e21c3153b85aacd9e269bb801ada
AmrKhaledSaleh <amrkalledsaleh1@gmail.com>
2023-12-17 03:38:06 +0200

    camera added

---
+++ GUI.py
@@ -0,0 +1,154 @@
+import tkinter as tk
+from tkinter import filedialog
+from inference import FER
+
+
+class GUI:
+    def __init__(self):
+
+        # Create the window and set properties
+        self.root = tk.Tk()
+        self.root.title("Face Expression Detector")
+        self.root.geometry("800x600")
+        self.root.configure(bg="#34495E")  # Set background color
+
+        # Configure a modern and elegant font
+        font_style = ("Helvetica", 12)
+
+        # Create app Frames
+        self.MainFrame = tk.Frame(self.root, bg="#34495E")
+        self.ModelFrame = tk.Frame(self.root, bg="#34495E")
+        self.PhotoFrame = tk.Frame(self.root, bg="#34495E")
+        self.VideoFrame = tk.Frame(self.root, bg="#34495E")
+        self.CameraFrame = tk.Frame(self.root, bg="#34495E")
+
+        # Home Button to get back to MainFrame
+        self.home = tk.Button(self.root, text="Home", font=font_style, command=self.Home, bg="#2ECC71", fg="#FFFFFF")
+        self.home.place(x=10, y=10, height=40, width=80)
+
+        # model Variable contains the model as a string "ANN" or "CNN"
+        self.model = ""
+
+        # Call the main frame to start the app
+        self.Make_ModelFrame()
+
+        # Load the app
+        self.root.mainloop()
+
+    def set_model_cnn(self):
+        self.model = "cnn"
+        self.ModelFrame.pack_forget()
+        self.Make_MainFrame()
+        print("Selected model: CNN")
+
+    def set_model_ann(self):
+        self.model = "ann"
+        self.ModelFrame.pack_forget()
+        self.Make_MainFrame()
+        print("Selected model: ANN")
+
+    def Home(self):
+        self.ModelFrame.pack()
+        self.MainFrame.pack_forget()
+        self.PhotoFrame.pack_forget()
+        self.VideoFrame.pack_forget()
+        self.CameraFrame.pack_forget()
+
+    def Make_MainFrame(self):
+        self.MainFrame.configure(bg="#34495E")
+
+        # the label on the top
+        self.Mainlabel = tk.Label(self.MainFrame, text="Choose Input Type", font=("Arial", 36), bg="#34495E", fg="#FFFFFF")
+
+        '''
+            - each button when you click it, Calls the new frame and makes the MainFrame invisible
+            - each frame calls the Make_ModelFrame that is responsible for choosing the model
+        '''
+        self.PhotoButton = tk.Button(self.MainFrame, text="Photo", font=("Arial", 24), command=self.Make_PhotoFrame,
+                                     bg="#3498DB", fg="#FFFFFF")
+        self.VideoButton = tk.Button(self.MainFrame, text="Video", font=("Arial", 24), command=self.Make_VideoFrame,
+                                     bg="#3498DB", fg="#FFFFFF")
+        self.CameraButton = tk.Button(self.MainFrame, text="Camera", font=("Arial", 24), command=self.Make_CameraFrame,
+                                      bg="#3498DB", fg="#FFFFFF")
+
+        self.Mainlabel.pack(pady=80)
+        self.PhotoButton.pack(pady=30)
+        self.VideoButton.pack(pady=30)
+        self.CameraButton.pack(pady=30)
+
+        self.MainFrame.pack()
+
+    def Make_ModelFrame(self):
+        self.ModelFrame.configure(bg="#34495E")
+
+        self.Modellabel = tk.Label(self.ModelFrame, text="Choose Model", font=("Arial", 36), bg="#34495E", fg="#FFFFFF")
+
+        self.ModelANN = tk.Button(self.ModelFrame, text="ANN", font=("Arial", 24), command=self.set_model_ann,
+                                  bg="#3498DB", fg="#FFFFFF")
+        self.ModelCNN = tk.Button(self.ModelFrame, text="CNN", font=("Arial", 24), command=self.set_model_cnn,
+                                  bg="#3498DB", fg="#FFFFFF")
+
+        self.Modellabel.pack(pady=100)
+        self.ModelANN.pack(pady=30)
+        self.ModelCNN.pack(pady=30)
+
+        self.ModelFrame.pack()
+
+    def open_file_explorer(self):
+        file_path = filedialog.askopenfilename(title="Select Photo",
+                                               filetypes=[("Image Files", "*.jpg *.jpeg *.png")])
+
+        # Update the photo path entry with the selected file path
+        self.photo_path_entry.configure(state="normal")
+        self.photo_path_entry.delete(0, tk.END)
+        self.photo_path_entry.insert(0, file_path)
+        self.photo_path_entry.configure(state="readonly")
+
+    def Make_PhotoFrame(self):
+        self.MainFrame.pack_forget()
+
+        # Set background color for PhotoFrame
+        self.PhotoFrame.configure(bg="#34495E")
+
+        # Button for browsing photo
+        self.browse_photo_button = tk.Button(self.PhotoFrame, text="Browse Photo", font=("Arial", 20),
+                                             command=self.open_file_explorer, bg="#3498DB", fg="#FFFFFF")
+        self.browse_photo_button.pack(pady=20)
+
+        # Label to display selected photo path
+        self.photo_path_label = tk.Label(self.PhotoFrame, text="Selected Photo Path:", font=("Arial", 16),
+                                         bg="#34495E", fg="#FFFFFF")
+        self.photo_path_label.pack(pady=10)
+
+        # Placeholder for displaying the selected photo path
+        self.photo_path_entry = tk.Entry(self.PhotoFrame, font=("Arial", 16), state="normal")
+        self.photo_path_entry.pack(pady=10)
+
+        self.PhotoFrame.pack()
+
+    def Make_VideoFrame(self):
+        self.MainFrame.pack_forget()
+
+        # Set background color for VideoFrame
+        self.VideoFrame.configure(bg="#34495E")
+
+        # hey, model variable has the selected model as a string
+        # Write your code here bro
+
+        self.VideoFrame.pack()
+
+    def Make_CameraFrame(self):
+        self.MainFrame.pack_forget()
+
+        # Set background color for CameraFrame
+        self.CameraFrame.configure(bg="#34495E")
+
+        # hey, model variable has the selected model as a string
+        # Write your code here bro
+        fer = FER(model_type=self.model)
+        fer.run()
+
+        self.CameraFrame.pack()
+
+
+GUI()
---
+++ inference.py
@@ -0,0 +1,121 @@
+import cv2
+import numpy as np
+from keras.models import model_from_json
+
+
+class FER:
+    def __init__(self, video_source=None, model_type='cnn'):
+        self.face_detector = cv2.CascadeClassifier('models/haarcascade_frontalface_default.xml')
+        self.label_dict = {0: 'anger', 1: 'disgust', 2: 'fear', 3: 'happiness', 4: 'neutral', 5: 'sad', 6: 'surprise'}
+
+        # Create a VideoCapture object
+        self.cap = cv2.VideoCapture(video_source) if video_source else cv2.VideoCapture(0)
+        if not self.cap.isOpened():
+            print("Error: Could not open video source.")
+            exit()
+
+        # Define the window name
+        self.window_name = 'VideoCapture'
+
+        # Load the specified model
+        if model_type == 'cnn':
+            self.expression_detector = self.load_cnn_model()
+        elif model_type == 'ann':
+            self.expression_detector = self.load_ann_model()
+        else:
+            raise ValueError("Invalid model type. Use 'cnn' or 'ann'.")
+
+        # Create a named window
+        cv2.namedWindow(self.window_name, cv2.WINDOW_NORMAL)
+        cv2.resizeWindow(self.window_name, 800, 750)
+
+    @staticmethod
+    def load_cnn_model():
+        # Loading CNN Trained Model
+        cnn_model_path = 'models/FER/cnn/model.json'
+        cnn_weights_path = 'models/FER/cnn/weights.h5'
+
+        with open(cnn_model_path, 'r') as json_file:
+            cnn_model_json = json_file.read()
+
+        cnn_model = model_from_json(cnn_model_json)
+        cnn_model.load_weights(cnn_weights_path)
+
+        return cnn_model
+
+    @staticmethod
+    def load_ann_model():
+        # Loading ANN Trained Model
+        ann_model_path = 'models/FER/ann/model.json'
+        ann_weights_path = 'models/FER/ann/weights.h5'
+
+        with open(ann_model_path, 'r') as json_file:
+            ann_model_json = json_file.read()
+
+        ann_model = model_from_json(ann_model_json)
+        ann_model.load_weights(ann_weights_path)
+
+        return ann_model
+
+    def process_frame(self, frame):
+        # Convert the frame to grayscale for face detection
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Detect faces in the frame
+        faces = self.face_detector.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
+
+        for (x, y, w, h) in faces:
+            # Extract the region of interest (ROI)
+            face_roi = gray_frame[y:y + h, x:x + w]
+
+            # Expand dimensions to make it compatible with model input shape (add batch dimension)
+            img = np.expand_dims(np.expand_dims(cv2.resize(face_roi, (48, 48)), -1), 0)
+
+            # Make a prediction using the model
+            expression_prediction = self.expression_detector.predict(img)
+
+            # Get the corresponding emotion label from the dictionary
+            emotion_label = self.label_dict[np.argmax(expression_prediction)]
+
+            # Draw a rectangle around the detected face
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (250, 13, 12), thickness=3)
+
+            # Display the predicted expression
+            img = frame
+            txt = f"Expression: {emotion_label}"
+            position = (x, y - 10)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            font_scale = 0.5
+            txt_color = (0, 0, 0)
+            outline_color = (255, 255, 255)
+            cv2.putText(img, txt, position, font, font_scale, outline_color, 5)
+            cv2.putText(img, txt, position, font, font_scale, txt_color, 2)
+
+        # Display the frame in a window
+        cv2.imshow(self.window_name, frame)
+
+    def run(self):
+        while True:
+            # Read a frame from the camera
+            ret, frame = self.cap.read()
+
+            if not ret:
+                print("Error: Failed to read frame.")
+                break
+
+            self.process_frame(frame)
+
+            # Break the loop if the 'q' key is pressed
+            key = cv2.waitKey(1)
+            if key & 0xFF == ord('q') or key == 27:  # 'q' key or 'Esc' key
+                break
+
+        # Release the VideoCapture object and close the window
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+# Example usage:
+# detector_cnn = FER(model_type='cnn')  # For CNN model
+# detector_ann = FaceExpressionDetector(model_type='ann')  # For ANN model
+# detector_cnn.run()
+# detector_ann.run()
